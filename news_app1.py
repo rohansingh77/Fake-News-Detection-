1d51a7458f90becd7e186ed78d7dc3fa28bba1c4
rohansingh77 <107630597+rohansingh77@users.noreply.github.com>
2022-08-14 01:03:00 +0530

    Add files via upload

---
+++ news_app1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  8 23:43:41 2022
+
+@author: Sony
+"""
+import pandas as pd
+import re
+import streamlit as st 
+import pickle
+from nltk.stem.porter import PorterStemmer
+
+vectorizer=pickle.load(open('vectorizer.pkl','rb'))
+model=pickle.load(open('bg_model.pkl','rb'))
+port_stem = PorterStemmer()
+stopwords=['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+def stemming(content):
+    stemmed_content = re.sub('[^a-zA-Z]',' ',content)
+    stemmed_content = stemmed_content.lower()
+    stemmed_content = stemmed_content.split()
+    stemmed_content = [port_stem.stem(word) for word in stemmed_content if not word in stopwords]
+    stemmed_content = ' '.join(stemmed_content)
+    return stemmed_content
+
+
+st.title("Fake News Detection")
+input_news=st.text_area("Enter the News")
+if st.button('predict'):
+    #input_news = ["Pamela Geller Congress: Attorney General Lynch ‘Pleads Fifth’ on Secret Iran ‘Ransom’ Payments"]
+    #df2= pd.DataFrame(input_news)
+    #df2[0] =df2[0].apply(stemming)
+    input_news=stemming(input_news)
+    inputs= vectorizer.transform([input_news])
+    prediction = model.predict(inputs)
+    print(prediction)
+    if (prediction[0]==0):
+        st.header('The News is Real')
+    else:
+        st.header('The News is Fake')
+   
